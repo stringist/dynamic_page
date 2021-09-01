8addc7e9ee58cb5d6ee80ba86b1cb7d2066ee50b
Kalle Tiihonen <kall1429@stud.kea.dk>
2021-09-01 09:33:06 +0200

    lots more js productList

--- productList.js
+++ productList.js
@@ -15,12 +15,27 @@ function handleProductList(data) {
 
 function showProduct(product) {
     //grab template
-
+    const template = document.querySelector("#productCardTemplate").content;
     //clone it
-
+    const copy = template.cloneNode(true);
     //change content
 
     //grab parent
-
+    const parent = document.querySelector("main");
     //append}
+    parent.appendChild(copy);
 }
+
+/* <template id="productCardTemplate"><a href="product_page.html?id=">
+        <article>
+          <h1>product name</h1>
+          <img
+            src="https://kea-alt-del.dk/t7/images/webp/640/1163.webp"
+            alt="shirt"/>
+          <p>
+            description
+          </p>
+          <h2><span>price</span>dkk</h2>
+          <button>View</button>
+        </article>
+      </a></template> */
