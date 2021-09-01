84f3c0c8291a5ab0b0f023128ca8ad98576e9919
Kalle Tiihonen <kall1429@stud.kea.dk>
2021-09-01 09:21:08 +0200

    productlis scaffold JS

---
+++ productList.js
@@ -0,0 +1,26 @@
+const url = "https://kea-alt-del.dk/t7/api/products";
+
+fetch(url)
+    .then(function(res) {
+        return res.json();
+    })
+    .then(function(data) {
+        handleProductList(data);
+    });
+
+function handleProductList(data) {
+    //   console.log(data);
+    data.forEach(showProduct);
+}
+
+function showProduct(product) {
+    //grab template
+
+    //clone it
+
+    //change content
+
+    //grab parent
+
+    //append}
+}
