366930d467369baf8c52e63bb49e380fe3892dbd
Kalle Tiihonen <kall1429@stud.kea.dk>
2021-09-01 14:21:57 +0200

    fixed description html

--- productView.js
+++ productView.js
@@ -12,7 +12,7 @@ function showProduct(product) {
     console.log(product);
     document.querySelector("h1.product_name").textContent =
         product.productdisplayname;
-    document.querySelector("p.product_headline").textContent = product.styledesc;
+    document.querySelector("p.product_headline").innerHTML = product.styledesc;
     document.querySelector("p.product_description").innerHTML =
         product.description;
     document.querySelector("img.product_image").src = `https://kea-alt-del.dk/t7/images/webp/1000/${product.id}.webp`;
